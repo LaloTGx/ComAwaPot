65082d586cc4dc8bad89b7918f962186e7f27112
LaloTGx <89763322+LaloTGx@users.noreply.github.com>
2026-09-09 22:42:33 -0600

    Feat: Define entity relationships | Add index | Add DeleteBehavior
    Agregue en el archivo AppDbContext las relaciones entre los modelos que
    creamos anteriormente ademas de agregar deletebehavior para proteger el
    historial para cada persona, agregue tambien los indices para las
    busquedas, tambien agregue unos parametros que recibira las cantidades
    de los costos (18,2), con esto se podra crear la primera version de la
    base de datos :)

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -23,4 +23,84 @@ public class AppDbContext : DbContext
     {
         optionsBuilder.UseSqlite("Data Source=ComAwaPot.db");
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+	    // Persona
+	    modelBuilder.Entity<Persona>()
+	        .HasMany(p => p.HistorialSituaciones)
+	        .WithOne(h => h.Persona)
+	        .HasForeignKey(h => h.IdPersona);
+            .OnDelete(DeleteBehavior.Restrict);
+
+	    modelBuilder.Entity<Persona>()
+	        .HasMany(p => p.PagosTarifa)
+	        .WithOne(p => p.Persona)
+	        .HasForeignKey(p => p.IdPersona);
+            .OnDelete(DeleteBehavior.Restrict);
+
+	    modelBuilder.Entity<Persona>()
+	        .HasMany(p => p.PagosAportacion)
+	        .WithOne(p => p.Persona)
+	        .HasForeignKey(p => p.IdPersona);
+            .OnDelete(DeleteBehavior.Restrict);
+
+	    // PagoTarifa
+	    modelBuilder.Entity<PagoTarifa>()
+	        .HasMany(p => p.Detalles)
+	        .WithOne(d => d.PagoTarifa)
+	        .HasForeignKey(d => d.IdPagoTarifa);
+            .OnDelete(DeleteBehavior.Cascade);
+
+	    // DetallePagoTarifa
+	    modelBuilder.Entity<DetallePagoTarifa>()
+	        .HasOne(d => d.Tarifa)
+	        .WithMany()
+	        .HasForeignKey(d => d.IdTarifa);
+            .OnDelete(DeleteBehavior.Restrict);
+
+	    // AportacionExtraordinaria
+	    modelBuilder.Entity<AportacionExtraordinaria>()
+	        .HasMany(a => a.Pagos)
+	        .WithOne(p => p.AportacionExtraordinaria)
+	        .HasForeignKey(p => p.IdAportacionExtraordinaria);
+            .OnDelete(DeleteBehavior.Restrict);
+
+	    // Índices de Persona
+	    modelBuilder.Entity<Persona>()
+	        .HasIndex(p => p.Nombre);
+
+	    modelBuilder.Entity<Persona>()
+	        .HasIndex(p => p.Calle);
+
+	    // Tarifa: no puede haber dos tarifas para el mismo mes
+	    modelBuilder.Entity<Tarifa>()
+	        .HasIndex(t => t.Periodo)
+	        .IsUnique();
+
+	    // Precisión de montos
+	    modelBuilder.Entity<Tarifa>()
+	        .Property(t => t.MontoMensual)
+	        .HasPrecision(18, 2);
+
+	    modelBuilder.Entity<PagoTarifa>()
+	        .Property(p => p.MontoTotal)
+	        .HasPrecision(18, 2);
+
+	    modelBuilder.Entity<DetallePagoTarifa>()
+	        .Property(d => d.MontoAplicado)
+	        .HasPrecision(18, 2);
+
+	    modelBuilder.Entity<AportacionExtraordinaria>()
+	        .Property(a => a.MontoTotal)
+	        .HasPrecision(18, 2);
+
+	    modelBuilder.Entity<AportacionExtraordinaria>()
+	        .Property(a => a.MontoPorPersona)
+	        .HasPrecision(18, 2);
+
+	    modelBuilder.Entity<PagoAportacion>()
+	        .Property(p => p.MontoPagado)
+	        .HasPrecision(18, 2);
+	}
 }
